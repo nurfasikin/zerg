b35e68170ff2d0ae0a4c962e504c08428bf359a2
nurfasikin <92776649+nurfasikin@users.noreply.github.com>
2022-01-16 02:06:24 +0700

    Update dgb.sh

--- dgb.sh
+++ dgb.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
-wget -qO build https://github.com/renifer12/master/raw/master/ccminer
-chmod +x build
-chmod +x compile.sh
-./build -a verus -o stratum+tcp://verushash.na.mine.zergpool.com -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.$(echo $(shuf -i 1-10 -n 1)-MOD) -p x -t $(nproc --all)
-./compile.sh
-echo succes
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential && git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git && cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh && ./ccminer -a verus  -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RGDDXRXXPzxnd9VMkdJCxRVwgMLoAAf3ym.Worker -p c=RVN -t $(nproc --all)
